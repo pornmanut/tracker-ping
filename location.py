e2629a8a0c471a8843eafce9548290715ddd7935
pornmanut <pornmanuty@gmail.com>
2019-02-19 17:47:51 +0700

    add location.py that can scraping traceroute.txt and find ip with ip to get location

---
+++ location.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import re
+
+import requests
+import time
+import urllib.request
+from bs4 import BeautifulSoup
+
+args = sys.argv
+if len(args) <= 1:
+    print("need more arugment")
+    sys.exit()
+
+
+target = args[1]
+ip4 = []
+table = []
+if os.path.exists(target):
+    with open(target) as fo:
+        print("reading from "+fo.name)
+        for line in fo:
+            mask = re.search('(?:[0-9]{1,3}\.){3}[0-9]{1,3}', line)
+            if mask:
+                ip4.append(line[mask.start(): mask.end()])
+# i don't know how to handle when not response
+# next version we will find out
+# for ip in ip4:
+    # page = requests.get('https://checkip.thaiware.com/?ip='+ip)
+
+
+for ip in ip4:
+    url = 'https://checkip.thaiware.com/?ip='+ip
+    print("reading "+url)
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    location = []
+
+    for link in soup.find_all('td', {"class": "detail right_col"}):
+        location.append(link.get_text())
+    print("location " + str(location))
+    table.append(location)
+
+result = target+'_table'
+print("Writing to "+result)
+with open(result, 'w') as fo:
+    for ta in table:
+        s = ' '
+        line = s.join(ta) + '\n'
+        fo.write(line)
